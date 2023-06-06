bca6c7515805679915d1efbd2c608c64dd618755
Niklas <niklas@kizo-agency.com>
2023-06-06 21:40:21 +0200

    Update connectToDb function to be able to work in prod and dev

--- src/utils/connectToDb.js
+++ src/utils/connectToDb.js
@@ -2,12 +2,11 @@ import { Client } from "pg";
 
 export default async function connectToDb() {
   "use server";
+  const isProd = process.env.NODE_ENV === "production";
+
   const client = new Client({
-    user: process.env.PG_USER,
-    host: process.env.PG_HOST,
-    database: process.env.PG_DATABASE,
-    password: process.env.PG_PASSWORD,
-    port: process.env.PG_PORT,
+    connectionString: process.env.DATABASE_URL,
+    ssl: isProd ? { rejectUnauthorized: false } : false,
   });
 
   await client.connect();
